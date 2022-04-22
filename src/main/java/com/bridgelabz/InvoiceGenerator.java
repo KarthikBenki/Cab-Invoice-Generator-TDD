7513559e2847a895f8820b0c50e2b58efc82886d
KarthikBenki <karthikmc007@gmail.com>
2022-04-22 12:22:12 +0530

    Added code and test for calculating minimum fare for a ride

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,10 +1,18 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-    private static final double MINIMUM_COST_PER_KM = 10;
-    private static final int COST_PER_TIME = 1;
+  private static final double MINIMUM_COST_PER_KM = 10; // 10RS PER KM
+  private static final int COST_PER_TIME = 1; // 1RS PER MINUTE
+  private static final double MINIMUM_FARE = 5; // minimum fare is 5
 
-    public double calculateFare(double distance, int time) {
-        return distance*MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+  /*
+  method to calculate total fare
+   */
+  public double calculateFare(double distance, int time) {
+    double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+    if (totalFare < MINIMUM_FARE) {
+      return MINIMUM_FARE;
     }
+    return totalFare;
+  }
 }
