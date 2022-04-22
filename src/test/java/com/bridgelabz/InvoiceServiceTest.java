954f81251dc41669ff882f47936583963d999bea
KarthikBenki <karthikmc007@gmail.com>
2022-04-22 12:37:17 +0530

    Added code and test for calculating total fare for multiple rides

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -4,24 +4,42 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
-    /*
-    test to calculate total fare
-     */
+  /*
+  test to calculate total fare
+   */
   @Test
   public void givenDistanceAndTime_ShouldReturnTotalFare() {
-      InvoiceGenerator invoiceGenerator = new InvoiceGenerator();//creating invoice generator object
-      double distance = 2.0;
-      int time = 5;
-      double fare = invoiceGenerator.calculateFare(distance, time);//calculating fare by calling method
-      Assertions.assertEquals(25,fare);//comparing expected and actual
+    InvoiceGenerator invoiceGenerator = new InvoiceGenerator(); // creating invoice generator object
+    double distance = 2.0;
+    int time = 5;
+    double fare =
+        invoiceGenerator.calculateFare(distance, time); // calculating fare by calling method
+    Assertions.assertEquals(25, fare); // comparing expected and actual
   }
 
+  /*
+  test to calculate minimum fare
+   */
   @Test
   public void givenLessDistanceAndTime_ShouldReturnTrue() {
-      InvoiceGenerator invoiceGenerator = new InvoiceGenerator();//creating invoice generator object
-      double distance = 0.1;
-      int time = 1;
-      double fare = invoiceGenerator.calculateFare(distance, time);//0.1*10+1*1=2;
-      Assertions.assertEquals(5,fare);//expected is 5 as minimum fare is five
+    InvoiceGenerator invoiceGenerator = new InvoiceGenerator(); // creating invoice generator object
+    double distance = 0.1;
+    int time = 1;
+    double fare = invoiceGenerator.calculateFare(distance, time); // 0.1*10+1*1=2;
+    Assertions.assertEquals(5, fare); // expected is 5 as minimum fare is five
   }
+
+  /*
+test to calculate fare for multiple rides
+   */
+  @Test
+  public void givenMultipleRides_ShouldReturnTotalFare() {
+    InvoiceGenerator invoiceGenerator = new InvoiceGenerator(); // creating invoice generator object
+    Ride[] rides = {new Ride(2.0, 5),
+                    new Ride(0.1,1)
+    };
+    double totalFare = invoiceGenerator.calculateFare(rides);
+    Assertions.assertEquals(30,totalFare);
+  }
+
 }
